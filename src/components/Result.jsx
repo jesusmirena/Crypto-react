ac16faf45325b72cca5813a5403136394bfd5553
jesusmirena <jesusamh16@gmail.com>
2022-07-09 01:45:41 -0500

    Formating and styling the result

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,27 +1,61 @@
 import styled from "@emotion/styled";
 
+const Container = styled.div`
+  color: #fff;
+  font-family: "Lato", sans-serif;
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+  margin-top: 30px;
+`;
+
+const Image = styled.img`
+  display: block;
+  width: 120px;
+`;
+
+const Text = styled.p`
+  font-size: 18px;
+  span {
+    font-weight: 700;
+  }
+`;
+
+const Price = styled.p`
+  font-size: 30px;
+  span {
+    font-weight: 700;
+  }
+`;
+
 const Result = ({ result }) => {
   const { PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE } =
     result;
 
   return (
-    <div>
-      <p>
-        The price is: <span>{PRICE}</span>
-      </p>
-      <p>
-        The highest price of the day is: <span>{HIGHDAY}</span>
-      </p>
-      <p>
-        The lowest price of the day is: <span>{LOWDAY}</span>
-      </p>
-      <p>
-        Change in last 24hours: <span>{CHANGEPCT24HOUR}</span>
-      </p>
-      <p>
-        Last update: <span>{LASTUPDATE}</span>
-      </p>
-    </div>
+    <Container>
+      <Image
+        src={`https://cryptocompare.com/${IMAGEURL}`}
+        alt="cryptocurrency"
+      />
+      <div>
+        <Price>
+          The price is: <span>{PRICE}</span>
+        </Price>
+        <Text>
+          The highest price of the day is: <span>{HIGHDAY}</span>
+        </Text>
+        <Text>
+          The lowest price of the day is: <span>{LOWDAY}</span>
+        </Text>
+        <Text>
+          Change in last 24hours: <span>{CHANGEPCT24HOUR}</span>
+        </Text>
+        <Text>
+          Last update: <span>{LASTUPDATE}</span>
+        </Text>
+      </div>
+    </Container>
   );
 };
 
