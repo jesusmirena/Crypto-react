6d659ad9d62ad4c03ac5e6b6a7408a1fedf3ac82
jesusmirena <jesusamh16@gmail.com>
2022-07-08 23:43:41 -0500

    Styling our select

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -14,6 +14,7 @@ const InputSubmit = styled.input`
   font-size: 20px;
   border-radius: 5px;
   transition: background-color 0.3s ease;
+  margin-top: 30px;
 
   &:hover {
     background-color: #7a7dfe;
--- src/hooks/useSelectCurrencies.jsx
+++ src/hooks/useSelectCurrencies.jsx
@@ -3,20 +3,32 @@ import styled from "@emotion/styled";
 
 const Label = styled.label`
   color: #fff;
+  display: block;
+  font-family: "Lato", sans-serif;
+  font-size: 24px;
+  font-weight: 700;
+  margin: 15px 0;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  font-size: 18px;
+  padding: 14px;
+  border-radius: 10px;
 `;
 
 const useSelectCurrencies = (label, options) => {
   const SelectCurrencies = () => (
     <>
       <Label htmlFor="">{label}</Label>
-      <select>
+      <Select>
         <option value="">Select a currency</option>
         {options.map((option) => (
           <option key={option.id} value={option.id}>
             {option.name}
           </option>
         ))}
-      </select>
+      </Select>
     </>
   );
   return [SelectCurrencies];
